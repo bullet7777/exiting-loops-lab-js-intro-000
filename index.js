825ffd462f67c7a5780759fc285686eb74caf196
Chris <chris.mahfouz@hotmail.com>
2021-02-21 12:35:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,18 @@ function breakOut(array,changeValue,stopValue){
 
 function keepGoing(array,changeValue,skipValue){
   for(let i=0;i<array.length;i++){
-    if(array[i]!==skipValue){
-      array[i]=changeValue
+    if(array[i]===skipValue){
+    continue  
     }
-
+array[i]=changeValue
 
   }
   return array
+}
+function findBy(array,findFn){
+  for(let i=0;i<array.length;i++){
+    if(findFn(array[i])){
+      return array[i]
+    }
+  }
 }
